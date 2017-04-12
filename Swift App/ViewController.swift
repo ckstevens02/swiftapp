1f627ecb2f7c66af5e10ed4a37f022b3787a793a
my first commit <Christina@Christinas-MacBook-Pro.local>
2017-04-12 16:37:46 +0900

    my first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelsOnLabels: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonPushed(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount % 2 == 1 {
+            labelsOnLabels.text = "TIK"
+        }
+        else{
+            labelsOnLabels.text = "TOK"
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGray
     }
 
     override func didReceiveMemoryWarning() {
